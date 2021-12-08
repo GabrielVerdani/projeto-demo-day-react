4f007b01431c556d49d5f6cb74ddfb29c9e3ac18
Rafael da Silva Santos <rafaelsilvarssantos@gmail.com>
2021-12-08 17:06:56 -0300

    ARRUMA AÍ VERDANI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
-import { FaSearch, FaStore } from 'react-icons/fa';
+import { FaSearch, FaStore, FaBars} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import Dropdown from "./Dropdown"
+import { useState } from 'react';
 
 //Import das Imagens
 import imageLogo from '../assets/image-logo-blue.png';
@@ -7,18 +9,27 @@ import textLogo from '../assets/text-logo-blue.png';
 import defaultProfile from '../assets/default-profile.png';
 
 export default function Header() {
+  const [dropdown, setDropdown] = useState(false);
   const nome = "Lucas";
 
   return (
     <div className="header">
       <div className="wrapper">
         <div className="left-side">
-          <Link to="/" className="logo">
+          <Link to="/" className="logo" >
             <img src={imageLogo} alt="Logo VOA" className="image-logo" />
             <img src={textLogo} alt="Logo VOA" className="text-logo" />
           </Link>
-            <p>teste</p>
+          <div onMouseEnter={() => setDropdown(true)} onMouseLeave={() => setDropdown(false)}>
+
+          <FaBars className="dropBarHamburguer"  />
+          {dropdown && <Dropdown /> }
+
+          </div>
+         
         </div>
+
+        
 
         <form action="/pesquisa">
           <input type="text" name="busca" id="searchBar" placeholder="Buscar..." required />
